b2479f6e695f825576cef4c23ee6d619042428c9
Irungaray <lionelirungaray@gmail.com>
2021-01-18 17:41:50 -0300

    Initial Commit

---
+++ pages/about/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../../components/Navbar/Navbar'
+
+const AboutPage = () => {
+  return (
+    <section>
+      <Navbar />
+      <h1>Videocards</h1>
+    </section>
+  )
+}
+
+export default AboutPage
---
+++ pages/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbar from '../components/Navbar/Navbar'
+
+const HomePage = () => {
+  return (
+    <div>
+      <Navbar />
+      <div>NEXT JS VIDEOCARDS</div>
+    </div>
+  )
+}
+
+export default HomePage
