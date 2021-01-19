789b0accf6e3789b86ee4ecf352cea47f4589b37
Irungaray <lionelirungaray@gmail.com>
2021-01-19 20:34:35 -0300

    Added Footer and About Us

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,11 +1,86 @@
-import React from 'react';
+import React from 'react'
+import Link from 'next/link'
+import { Segment, Container, Grid, List, Header } from 'semantic-ui-react'
 
-const Footer = () => {
-	return (
-		<div>
-      Footer
-		</div>
-	);
-};
+const Footer = () => (
+  <Segment
+    vertical
+    as="footer"
+    style={{
+      padding: '4em 0em',
+      marginTop: '3em',
+      borderTop: '1px solid #f2f2f2',
+    }}
+  >
+    <Container text>
+      <Grid stackable>
+        <Grid.Row>
+          <Grid.Column width={4}>
+            <Header as="h4" content="About" />
+            <List>
+              <List.Item>
+                <Link href="/about">
+                  <a>Us</a>
+                </Link>
+              </List.Item>
+            </List>
+          </Grid.Column>
+          <Grid.Column width={5}>
+            <Header as="h4" content="Products" />
+            <List>
+              <List.Item>
+                <Link href="/">
+                  <a>All our Products</a>
+                </Link>
+              </List.Item>
+            </List>
+          </Grid.Column>
+          <Grid.Column width={7}>
+            <Header as="h4">Done by</Header>
+            <p>
+              <a href="https://twitter.com/Irunga_Ray">Irungaray.{' '}</a>
+              My first incursion on NextJs! Hope you enjoy.
+            </p>
+            <List horizontal style={{ display: 'flex' }}>
+              <List.Item
+                icon="twitter"
+                style={{ display: 'flex' }}
+                content={<a href="https://twitter.com/Irunga_Ray">Twitter</a>}
+              />
+              <List.Item
+                icon="github"
+                style={{ display: 'flex' }}
+                content={
+                  <a href="https://github.com/Irungaray">
+                    GitHub
+                  </a>
+                }
+              />
+            </List>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+      <div className="colophon">
+        <p className="colophon-entry">
+				If you are a potential employer, thanks for your interest! Appreciate that you got all the way down here and read this.<br />
+				Please take me into consideration if you are looking to hire. <br />
+				Take care!
+        </p>
+      </div>
+    </Container>
 
-export default Footer;
+    <style jsx>{`
+      .colophon {
+        text-align: center;
+        margin-top: 3.2rem;
+        font-size: 0.8rem;
+      }
+      .colophon-entry {
+        color: grey;
+        margin-bottom: 0;
+      }
+    `}</style>
+  </Segment>
+)
+
+export default Footer
--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React from 'react'
+import { Image, Header } from 'semantic-ui-react'
+
+import Layout from '@components/Layout/Layout'
 
 const AboutPage = () => {
-	return (
-		<section>
-			<h1>Videocards</h1>
-		</section>
-	);
-};
+  return (
+    <Layout>
+      <Header as="h1" textAlign="center">
+        Really you got all the way down here?
+      </Header>
 
-export default AboutPage;
+      <h2>
+        Anyway, not much to see. Hope you enjoyed the other parts of the site!
+      </h2>
+
+      <h2>
+        Also take a look at this parallel project made by my company{' '}
+        <a href="https://github.com/Altair-Devs">Altaïr Devs</a> <br />
+      </h2>
+
+      <h2>
+        <a href="https://github.com/Altair-Devs/Renacimiento">
+          Renaissance Art
+        </a>
+      </h2>
+
+      <style jsx>{`
+        h2 {
+          text-align: center;
+        }
+      `}</style>
+    </Layout>
+  )
+}
+
+export default AboutPage
