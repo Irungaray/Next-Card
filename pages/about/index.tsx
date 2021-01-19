7ddcb9d51a45273920187461945de29052693c4e
Irungaray <lionelirungaray@gmail.com>
2021-01-19 06:43:07 -0300

    Layed out

--- pages/about/index.tsx
+++ pages/about/index.tsx
@@ -1,10 +1,8 @@
 import React from 'react'
-import Navbar from '../../components/Navbar/Navbar'
 
 const AboutPage = () => {
   return (
     <section>
-      <Navbar />
       <h1>Videocards</h1>
     </section>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState} from 'react'
-import Navbar from '../components/Navbar/Navbar'
 
 const HomePage = () => {
   const [productList, setProductList] = useState<TProduct[]>([]);
@@ -17,7 +16,6 @@ const HomePage = () => {
 
   return (
     <div>
-      <Navbar />
       <div>NEXT JS VIDEOCARDS</div>
 
       {productList.map((product) => (
